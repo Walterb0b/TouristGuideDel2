0198608ea528173d31734d42791dda45fd703abe
Walterb0b <osca0001@stud.ek.dk>
2026-02-10 20:53:12 +0100

    Initial commit

---
+++ src/test/java/com/touristguidedel2/touristguidedel2/TouristGuideDel2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touristguidedel2.touristguidedel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideDel2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
