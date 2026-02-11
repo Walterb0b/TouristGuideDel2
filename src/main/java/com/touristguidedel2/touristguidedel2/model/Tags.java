fad189f022745cfa6739912b5147796440ce20f1
Walterb0b <osca0001@stud.ek.dk>
2026-02-11 10:29:39 +0100

    Ændringer til dansk

--- src/main/java/com/touristguidedel2/touristguidedel2/model/Tags.java
+++ src/main/java/com/touristguidedel2/touristguidedel2/model/Tags.java
@@ -1,5 +1,20 @@
 package com.touristguidedel2.touristguidedel2.model;
 
 public enum Tags {
-    KID_FRIENDLY, FREE, RESTAURANT, MUSEUM, ENTERTAINMENT, HISTORY
+    KID_FRIENDLY("Børnevenlig"),
+    FREE("Gratis"),
+    RESTAURANT("Restaurant"),
+    MUSEUM("Museum"),
+    ENTERTAINMENT("Underholdning"),
+    HISTORY("Historie");
+
+    private final String displayName;
+
+    Tags(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
