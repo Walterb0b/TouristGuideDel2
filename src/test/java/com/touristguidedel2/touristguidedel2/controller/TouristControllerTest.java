71a0985b2c77045ed95f3319892ead5a306aa149
Walterb0b <osca0001@stud.ek.dk>
2026-02-13 15:10:51 +0100

    WebMvcTest på controller

---
+++ src/test/java/com/touristguidedel2/touristguidedel2/controller/TouristControllerTest.java
@@ -0,0 +1,69 @@
+package com.touristguidedel2.touristguidedel2.controller;
+
+import com.touristguidedel2.touristguidedel2.model.Cities;
+import com.touristguidedel2.touristguidedel2.model.Tags;
+import com.touristguidedel2.touristguidedel2.model.TouristAttraction;
+import com.touristguidedel2.touristguidedel2.service.TouristService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest
+class TouristControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TouristService touristService;
+
+    @Test
+    void getAllAttractions() throws Exception {
+        List<TouristAttraction> attractions = new ArrayList<>();
+        attractions.add(new TouristAttraction("Legoland", "Forlystelsespark", Cities.VEJLE, List.of(Tags.ENTERTAINMENT, Tags.KID_FRIENDLY)));
+
+        when(touristService.getAllAttractions()).thenReturn(attractions);
+
+        mockMvc.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attractionsList"))
+                .andExpect(model().attributeExists("attractions"))
+                .andExpect(model().attribute("attractions", attractions));
+    }
+
+    @Test
+    void saveAttraction() throws Exception{
+        TouristAttraction touristAttraction = new TouristAttraction("Legoland", "Forlystelsespark", Cities.VEJLE, List.of(Tags.ENTERTAINMENT, Tags.KID_FRIENDLY));
+
+        mockMvc.perform(post("/attractions/save")
+                .param("name", touristAttraction.getName())
+                .param("description", touristAttraction.getDescription())
+                .param("city", touristAttraction.getCity().toString())
+                .param("tags", touristAttraction.getTags().get(0).toString())
+                .param("tags", touristAttraction.getTags().get(1).toString()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/attractions"));
+
+        ArgumentCaptor<TouristAttraction> captor = ArgumentCaptor.forClass(TouristAttraction.class);
+        verify(touristService).addAttraction(captor.capture());
+
+        TouristAttraction captured = captor.getValue();
+        assertEquals("Legoland", captured.getName());
+        assertEquals("Forlystelsespark", captured.getDescription());
+        assertEquals(Cities.VEJLE, captured.getCity());
+        assertEquals(List.of(Tags.ENTERTAINMENT, Tags.KID_FRIENDLY), captured.getTags());
+
+    }
+}
